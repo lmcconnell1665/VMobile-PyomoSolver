df5af28196193ab5efd85b9f4d752ec9b74dc4be
msande44 <59846634+msande44@users.noreply.github.com>
2020-10-09 14:01:28 -0400

    Add files via upload

--- Project.py
+++ Project.py
@@ -9,16 +9,27 @@ ws=wb["Data"]
 model=AbstractModel()
 
 #Size
-model.nc=Param(initialize=3)
-model.nd=Param(initialize=5)
-model.ni=Param(initialize=3)
-model.nt=Param(initialize=2)
+model.nc=Param(initialize=3)#Number of carriers
+model.nd=Param(initialize=5)#Destinations
+model.ni=Param(initialize=3)#Price intervals
+model.nt=Param(initialize=2)#Months
 
 #Sets
-model.c=RangeSet(1, model.nc)
-model.d=RangeSet(1, model.nd)
-model.i=RangeSet(1, model.ni)
-model.t=RangeSet(1, model.nt)
+model.c=RangeSet(1, model.nc)#Carriers
+model.d=RangeSet(1, model.nd)#Destinations
+model.i=RangeSet(1, model.ni)#Price intervals
+model.t=RangeSet(1, model.nt)#Months
 
-#Param
+#Data
+#Price
+def p_init(model, c,d,i,t):
+    return (ws.cell(17+(3*(c-1))+i, 1+(5*(t-1))+d).value)
+model.p=Param(model.c, model.d, model.i ,model.t, initialize=p_init)
+
+#Penalty
+def pen_init(model, c,d,t):
+    return (ws.cell(30+c, 3*(t-1)+m+1).value)
+model.pen=Param(model.c, model.d, model.t, initialize=pen_init)
+
+
 
