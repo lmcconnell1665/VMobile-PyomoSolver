f2fa8ddd9546f21129f1959c47c7fafc619424c7
msande44 <59846634+msande44@users.noreply.github.com>
2020-10-09 14:21:24 -0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -31,5 +31,46 @@ def pen_init(model, c,d,t):
     return (ws.cell(30+c, 3*(t-1)+m+1).value)
 model.pen=Param(model.c, model.d, model.t, initialize=pen_init)
 
+#Demand Forcast
+def d_init(model,d,t):
+    return (ws.cell(35, 3*(t-1)+m+1).value)
+model.d=Param(model.d, model.t, initialize=d_init)
 
+#Lower Threshold
+def LT_init(model,c,i):
+    return (ws.cell(5+(3*(c-1))+i, 2).value)
+model.LT=Param(model.c, model.i, initialize=LT_init)
 
+#Upper Threshold
+def UT_init(model,c,i):
+    return (ws.cell(5+(3*(c-1))+i, 3).value)
+model.UT=Param(model.c, model.i, initialize=UT_init)
+
+#Lower Bound
+def LB_init(model,c,t):
+    return (ws.cell(30+c, 14+(2*(t-1)).value)
+model.LB=Param(model.c, model.t, initialize=LB_init)
+            
+#Upper Bound
+def UB_init(model,c,t):
+    return (ws.cell(30+c, 15+(2*(t-1)).value)
+model.UB=Param(model.c, model.t, initialize=UB_init)
+
+            
+#Variables
+model.X=Var(model.c, model.d, model.i, model.t, within=NonNegativeReals)
+model.bin=Var(model.c, model.i, within=NonNegativeReals)
+model.Z=Var(model.c, model.d, model.i, model.t, within=NonNegativeReals)
+            
+            
+#Expressions
+            
+            
+            
+            
+#OBJECTIVE
+
+            
+            
+
+#CONSTRAINTS
